7f9f5b56e545e8a59f8abb71ff9da16fc221c7dc
RyBuilds <Ryan@Registe.me>
2021-11-10 13:44:18 +0000

    toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-"use strict";
+'use strict';
+
+/* 
+TO DO
+  BODY NIGHTMODE STYLING
+DECISIONS 
+*/
+
+// const body = document.querySelector(`body`);
+const toggle = document.querySelector(`.toggle`);
+toggle.addEventListener(`click`, function () {
+  toggle.classList.toggle(`active`);
+  // body.classList.toggle(`active`);
+});
