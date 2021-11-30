161e4ce83e2de90e6a93fb7b9c2b5596a21e56a7
RyBuilds <Ryan@Registe.me>
2021-11-30 16:57:58 +0000

    spotify API, map, filter functionality, dark mode

---
+++ README.md
@@ -0,0 +1,8 @@
+<!-- THINK ABOUT -->
+
+TOGGLE AESTETICS
+COLOUR SCHEME FOR MAP
+
+I'm a self-taught front-end developer in London that knows my way around HTML, CSS and JavaScript. I'm 4 months into my software engineering journey, and building cool things are where my interests, passions and skills align. If you need a website or are looking to hire a junior software engineer, drop me a line!
+
+I'm a self-taught front-end developer from London. I'm 5 months into my software-engineering journey, and I know my way around HTML, CSS and JavaScript.
--- script.js
+++ script.js
@@ -1,14 +1,204 @@
 'use strict';
 
-/* 
-TO DO
-  BODY NIGHTMODE STYLING
-DECISIONS 
-*/
+//                            //
+//                            //
+// TOGGLE NIGHTMODE / DAYMODE //
+//                            //
+//                            //
 
-// const body = document.querySelector(`body`);
 const toggle = document.querySelector(`.toggle`);
+const body = document.querySelector(`body`);
+const navCta = document.querySelector(`body`);
+const card = document.querySelector(`.card`);
 toggle.addEventListener(`click`, function () {
-  toggle.classList.toggle(`active`);
-  // body.classList.toggle(`active`);
+  toggle.classList.toggle(`dark`);
+  body.classList.toggle(`dark`);
 });
+
+//                                //
+//                                //
+// MAIN FILTER MENU FUNCTIONALITY //
+//                                //
+//                                //
+
+const cardIntro = document.querySelector(`.card--intro`);
+const cardPhotos = document.querySelector(`.card--photos`);
+const cardMap = document.querySelector(`.card--map`);
+const cardSpotify = document.querySelector(`.card--spotify`);
+const cardRotation = document.querySelector(`.card--rotation`);
+const cardGuestbook = document.querySelector(`.card--guestbook`);
+const cardRunning = document.querySelector(`.card--running`);
+const cardGithub = document.querySelector(`.card--github`);
+const cardTwitter = document.querySelector(`.card--twitter`);
+const cardInstagram = document.querySelector(`.card--instagram`);
+const cardLinkedin = document.querySelector(`.card--linkedin`);
+const filterMain = document.querySelectorAll(`.filter`);
+const filterContainerMain = document.querySelector(`.filters-container-main`);
+
+filterContainerMain.addEventListener(`click`, function (event) {
+  const clicked = event.target.closest(`.filter`);
+  if (!clicked) return;
+  filterMain.forEach(filter => filter.classList.remove(`active`));
+  clicked.classList.add(`active`);
+  cardIntro.setAttribute(`id`, `card--intro--${clicked.dataset.filter}`);
+  cardPhotos.setAttribute(`id`, `card--photos--${clicked.dataset.filter}`);
+  cardMap.setAttribute(`id`, `card--map--${clicked.dataset.filter}`);
+  cardSpotify.setAttribute(`id`, `card--spotify--${clicked.dataset.filter}`);
+  cardRotation.setAttribute(`id`, `card--rotation--${clicked.dataset.filter}`);
+  cardRunning.setAttribute(`id`, `card--running--${clicked.dataset.filter}`);
+  cardGithub.setAttribute(`id`, `card--github--${clicked.dataset.filter}`);
+  cardTwitter.setAttribute(`id`, `card--twitter--${clicked.dataset.filter}`);
+  cardLinkedin.setAttribute(`id`, `card--linkedin--${clicked.dataset.filter}`);
+  cardGuestbook.setAttribute(
+    `id`,
+    `card--guestbook--${clicked.dataset.filter}`
+  );
+  cardInstagram.setAttribute(
+    `id`,
+    `card--instagram--${clicked.dataset.filter}`
+  );
+});
+
+//                                     //
+//                                     //
+// SECONDARY FILTER MENU FUNCTIONALITY //
+//                                     //
+//                                     //
+
+const filterSecondary = document.querySelectorAll(`.filter-secondary`);
+const filterContainerSecondary = document.querySelector(
+  `.filters-container-secondary`
+);
+const filterSecondaryContent = document.querySelectorAll(
+  `.filter-secondary-content`
+);
+filterContainerSecondary.addEventListener(`click`, function (event) {
+  const clicked = event.target.closest(`.filter-secondary`);
+  if (!clicked) return;
+  filterSecondary.forEach(filter => filter.classList.remove(`active`));
+  filterSecondaryContent.forEach(filter =>
+    filter.classList.remove(`filter-content--active`)
+  );
+  clicked.classList.add(`active`);
+  document
+    .querySelector(`.filter-content--${clicked.dataset.filter}`)
+    .classList.add(`filter-content--active`);
+});
+
+//             //
+//             //
+// MAP BOX API //
+//             //
+//             //
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicnlidWlsZHMiLCJhIjoiY2t3YjAyZWs1MGhibzJ2bXdldm9ieXo3ciJ9.Ofx9XCRLrLtO4mJkiu5rWg';
+const latitude = 51.49910499837011;
+const longitude = -0.1005871429501992;
+const coords = [longitude, latitude];
+
+function setupMap(coords) {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    // light-v10
+    // dark-v10
+    // streets-v11
+    attributionControl: false,
+    center: coords,
+    zoom: 13.15,
+  });
+
+  map.on('load', () => {
+    map.loadImage('img/memoji-map.png', (error, image) => {
+      if (error) throw error;
+      map.addImage('memoji', image);
+      map.addSource('point', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+              },
+            },
+          ],
+        },
+      });
+      map.addLayer({
+        id: 'points',
+        type: 'symbol',
+        source: 'point',
+        layout: {
+          'icon-image': 'memoji',
+          'icon-size': 0.25,
+        },
+      });
+    });
+  });
+
+  map.on('load', () => {
+    const layers = map.getStyle().layers;
+    const labelLayerId = layers.find(
+      layer => layer.type === 'symbol' && layer.layout['text-field']
+    ).id;
+    map.addLayer(
+      {
+        id: 'add-3d-buildings',
+        source: 'composite',
+        'source-layer': 'building',
+        filter: ['==', 'extrude', 'true'],
+        type: 'fill-extrusion',
+        minzoom: 9,
+        paint: {
+          'fill-extrusion-color': '#aaa',
+          'fill-extrusion-height': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            13,
+            0,
+            15.05,
+            ['get', 'height'],
+          ],
+          'fill-extrusion-base': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            13,
+            0,
+            15.05,
+            ['get', 'min_height'],
+          ],
+          'fill-extrusion-opacity': 0.6,
+        },
+      },
+      labelLayerId
+    );
+  });
+}
+setupMap(coords);
+
+//             //
+//             //
+// SPOTIFY API //
+//             //
+//             //
+
+const spotifyApiUrl = `http://registe-site-backend.herokuapp.com/v1/spotify`;
+
+async function getSpotifyData() {
+  const response = await fetch(spotifyApiUrl);
+  const data = await response.json();
+  const { name, url, imageUrl, previewUrl } = data;
+  document.getElementById(`spotify-song`).textContent = name;
+  document.getElementById(`spotify-song`).setAttribute(`href`, `${url}`);
+  document.getElementById(`spotify-artwork`).setAttribute(`src`, `${imageUrl}`);
+  document.getElementById(`spotify-artwork`).setAttribute(`href`, `${url}`);
+  // document.getElementById(`spotify-preview`).textContent = previewUrl;
+  console.log(name, url, imageUrl, previewUrl);
+}
+getSpotifyData();
