f3889065d45f3de1924e2bc360d361b4a13a3396
RyBuilds <Ryan@Registe.me>
2022-01-27 14:20:01 +0000

    resolved mapbox error

--- script.js
+++ script.js
@@ -104,46 +104,6 @@ function setupMap(coords) {
       });
     });
   });
-
-  map.on('load', () => {
-    const layers = map.getStyle().layers;
-    const labelLayerId = layers.find(
-      layer => layer.type === 'symbol' && layer.layout['text-field']
-    ).id;
-    // map.addLayer(
-    //   {
-    //     id: 'add-3d-buildings',
-    //     source: 'composite',
-    //     'source-layer': 'building',
-    //     filter: ['==', 'extrude', 'true'],
-    //     type: 'fill-extrusion',
-    //     minzoom: 9,
-    //     paint: {
-    //       'fill-extrusion-color': '#aaa',
-    //       'fill-extrusion-height': [
-    //         'interpolate',
-    //         ['linear'],
-    //         ['zoom'],
-    //         13,
-    //         0,
-    //         15.05,
-    //         ['get', 'height'],
-    //       ],
-    //       'fill-extrusion-base': [
-    //         'interpolate',
-    //         ['linear'],
-    //         ['zoom'],
-    //         13,
-    //         0,
-    //         15.05,
-    //         ['get', 'min_height'],
-    //       ],
-    //       'fill-extrusion-opacity': 0.6,
-    //     },
-    //   },
-    labelLayerId;
-    // );
-  });
 }
 setupMap(coords);
 
